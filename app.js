f2cb9f938fde1d41766edc6c7b602759b34d1f84
85Jakob <jacobadoney@gmail.com>
2022-08-11 16:18:47 -0500

    added history display

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-let currentOperation = null;
+let firstNumber = '';
+let secondNumber = '';
 
-const currentScreen = document.getElementById('current-screen')
+const mainScreen = document.getElementById('main-screen');
+const secondScreen = document.getElementById('second-screen');
 const numBtn = document.querySelectorAll('[data-number]');
 const oppBtn = document.querySelectorAll('[data-operator]');
 
-numBtn.forEach((button) => button.addEventListener('click', () => enterNumber(button.textContent)))
+numBtn.forEach((button) => button.addEventListener('click', () => enterNumber(button.textContent)));
+oppBtn.forEach((button) => button.addEventListener('click', () => setOperation(button.textContent)));
 
 function enterNumber(number) {
-    currentScreen.textContent += number
+    mainScreen.textContent += number;
+    firstNumber =  firstNumber + number;
+    console.log(firstNumber);
+}
+
+function setOperation(operator){
+    secondScreen.textContent = firstNumber + ' ' + operator;
+    
 }
