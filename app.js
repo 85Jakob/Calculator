f4ccc15844fade05a3c989cee8b13784e1cc5132
85Jakob <jacobadoney@gmail.com>
2022-08-11 17:01:03 -0500

    added operate function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let firstNumber = '';
-let secondNumber = '';
+let currentNumber = '';
+let prevNumber = '';
+let prevOperator = '';
 
 const mainScreen = document.getElementById('main-screen');
 const secondScreen = document.getElementById('second-screen');
@@ -10,12 +11,59 @@ numBtn.forEach((button) => button.addEventListener('click', () => enterNumber(bu
 oppBtn.forEach((button) => button.addEventListener('click', () => setOperation(button.textContent)));
 
 function enterNumber(number) {
-    mainScreen.textContent += number;
-    firstNumber =  firstNumber + number;
-    console.log(firstNumber);
+    mainScreen.textContent = '';
+    currentNumber =  currentNumber + number;
+    mainScreen.textContent = currentNumber;
 }
 
 function setOperation(operator){
-    secondScreen.textContent = firstNumber + ' ' + operator;
-    
+    if(prevNumber === ''){
+        secondScreen.textContent = currentNumber + ' ' + operator;
+        prevNumber = currentNumber;
+        currentNumber = '';
+        prevOperator = operator;
+        mainScreen.textContent = '';
+    }
+    else{
+        valid = operate(prevOperator);
+        secondScreen.textContent = prevNumber + ' ' + operator;
+        currentNumber = '';
+        prevOperator = operator;
+        if(valid === true){
+            mainScreen.textContent = '';
+        }
+        if(valid === false){
+            reset()
+        }
+    }
+}
+
+function operate(operator){
+    valid = true;
+    if(operator === 'x'){
+        prevNumber = '' + (parseInt(prevNumber) * parseInt(currentNumber));
+    }
+    else if(operator === '-'){
+        prevNumber = '' + (parseInt(prevNumber) - parseInt(currentNumber) );
+    }
+    else if(operator === '+'){
+        prevNumber = '' + (parseInt(prevNumber) + parseInt(currentNumber));
+    }
+    else if(operator === '÷'){
+        if(parseInt(currentNumber) === 0){
+            mainScreen.textContent = 'DIV/0';
+            valid = false;
+        }
+        else{
+            prevNumber = '' + (parseInt(prevNumber) / parseInt(currentNumber));
+        }
+    }
+    return valid;
+}
+
+function reset(){
+    secondScreen.textContent = ' ';
+    currentNumber = '';
+    prevNumber = '';
+    prevOperator = '';
 }
